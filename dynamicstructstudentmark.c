bdf8fa0d8eb9873f69d4a8b2e43521262452ff64
anupam1511 <158273618+anupam1511@users.noreply.github.com>
2024-02-16 11:17:20 +0530

    Update dynamicstructstudentmark.c

--- dynamicstructstudentmark.c
+++ dynamicstructstudentmark.c
@@ -1,14 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
 typedef struct student
 {
    char name[30] ;
    int *mark ;
+   float average ;
 }stu;
-int a ; stu *p;  int b ;
+int a ; stu *p;  int b ;float high = 0 , low = 0 ;
 void insert(void)
 {
-
+   srand(getpid());
    printf("enter the number of subject for student\n");
    scanf("%d",&b);
 
@@ -24,9 +28,15 @@ void insert(void)
              for(int j = 0 ; j < b ; j++)
              {
                  printf("enter the marks of subject %d :", j);
-                 scanf("%d" , &p[i].mark[j]);
+                // scanf("%d" , &p[i].mark[j]);
+                 p[i].mark[j] = rand()%1000 ;
              }
+
     }
+
+
+
+
 }
 void print()
 {
@@ -39,12 +49,50 @@ void print()
              printf(" marks of subject %d :", j);
              printf("%d\n" , p[i].mark[j]);
          }
+         printf("%.2f",p[i].average);
          printf("\n\n");
         }
+        printf("highest per : %.2f  lowest per %.2f" , high , low);
 
+}
+void average()
+{    float sum = 0 ;
+     for(int i = 0 ; i < a ; i++)
+     {
+
+      for(int j = 0 ; j < b ; j++)
+      {
+         sum +=  p[i].mark[j];
+      }
+      p[i].average = sum/b;
+
+     }
+
+}
+void highlow()
+{
+     for(int i = 0 ; i < a ; i++)
+     {
+        if(high<p[i].average)
+        {
+            high = p[i].average ;
+        }
+     }
+     low = p[0].average ;
+     for(int i  = 0  ; i < a-1  ; i++)
+     {
+        if(low  > p[i+1].average)
+        {
+                                                                                                                                                                                                                                                                                                                                                     12,51         Top
+  low = p[i].average ;
+        }
+     }
 }
 int main()
 {
      insert();
-     print() ;
+     average() ;
+     highlow();
+     print();
+
 }
